ce702b654e5dc8dab0d4b75ac621a9da87111b64
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-05-01 10:27:08 +0600

    powerFunction in c++

--- powerFunction.cpp
+++ powerFunction.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
 
 int main(){
-  double result = pow(lcpp_x:2, lcpp_y:3);
+  double lcpp_x = 2;
+  double lcpp_y = 3;
+  double result = pow(lcpp_x, lcpp_y);
   cout << result;
   return 0;
 }
